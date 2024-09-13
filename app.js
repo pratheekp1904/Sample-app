cbbf3dc5f9395dff40ee5d47364b4228301da1de
pratheekp1904 <pratheekpganiga@gmail.com>
2024-09-14 03:42:26 +0530

    Initial commit - Sample-app

---
+++ app.js
@@ -0,0 +1,21 @@
+// Load the Express module
+const express = require('express');
+
+// Create an Express application
+const app = express();
+
+// Define a route for the root URL
+app.get('/', (req, res) => {
+    res.send('Hello, World! This is my first Node.js app.');
+});
+
+// Another route
+app.get('/about', (req, res) => {
+    res.send('This is the About page of my first Node.js app.');
+});
+
+// Start the server
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
